3a328e98d9ea3a7f1f877c252b52d87d2306aedf
PascualBT <78772600+PascualBT@users.noreply.github.com>
2021-03-30 13:22:11 -0600

    Cambio3

--- Escenario/main.cpp
+++ Escenario/main.cpp
@@ -1,5 +1,3 @@
-
-
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
@@ -7,33 +5,36 @@
 #endif
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include "Operador.h"
-#include "Prisma.h"
+#include "Cubo.h"
 #include "Piramide.h"
-void init(void)
-{
-     //Establece el color de la ventana de visualizacion
-     //Los tres primeros parametros corresponden al RGB
-     //El cuarto parametro corresponde al valor alfa, que permite el efecto de transparencias
-     //0: objeto totalmente transparente; 1: objeto totalmente opaco
-     glClearColor(0.0, 0.0, 0.0, 0.0);
-     //establece los parametros de proyeccion ortogonal
-     //Se visualizara una proyeccion bidimensional de dimensiones 200 x 150
-     //(0,0): esquina inferior izquierda: punto de referencia de esta ventana
-     glMatrixMode(GL_PROJECTION);
-     glLoadIdentity();
-     gluOrtho2D(-540,540,-325,325);
-     glMatrixMode(GL_MODELVIEW);
-     glLoadIdentity();
- }
-void graficar()
-{
-    Operador ope;
-    Prisma cubo1;
-    Piramide piramide1;
 
+GLfloat anguloCuboX = 0.0f;
+GLfloat anguloCuboY = 0.0f;
+GLfloat anguloEsfera = 0.0f;
+
+GLint ancho, alto;
+int hazPerspectiva = 0;
+Cubo cubo1;
+Piramide piramide1;
+
+void reshape(int width, int height)
+{
+    glViewport(0, 0, width, height); //Puerto de visualizacion
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if(hazPerspectiva)
+      gluPerspective(60.0f, (GLfloat)width/(GLfloat)height, 1.0f, 20.0f);
+    else
+      glOrtho(-4, 4, -4, 4, 1, 10);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    ancho = width;
+    alto = height;
+}
+
+void drawCube(void)
+{
     cubo1.setPoints(0,0,0,0);
     cubo1.setPoints(1,0,0,1);
     cubo1.setPoints(1,1,0,2);
@@ -49,26 +50,74 @@ void graficar()
     piramide1.setPoints(0,0,-1,3);
     piramide1.setPoints(0.5,1,-0.5,4);
     piramide1.Draw();
-
-    ope.LoadIdenty();
-    ope.imprimir();
 }
- int main(int argc, char** argv)
- {
-      //se inicializa la pantalla grafica
-      glutInit(&argc, argv);
-      //establece el modo de visualizacion
-      glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-      //Establece la posicion de la esquina superior izquierda del grafico en la pantalla
-      glutInitWindowPosition(100,0);
-      //se establece el ancho y la altura de la ventana de visualizacion
-      glutInitWindowSize(1080,650);
-      //se crea la ventana de visualizacion
-      glutCreateWindow("Escenario");
-      //se ejecuta la funcion de inicializacion de parametros
-      init();
-      //se envian los graficos a pantalla
-      glutDisplayFunc(graficar);
-      glutMainLoop();
-      return 0;
-  }
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glLoadIdentity();
+    glTranslatef(0.0f, 0.0f, -5.0f);
+    glRotatef(anguloCuboX, 1.0f, 0.0f, 0.0f);
+    glRotatef(anguloCuboY, 0.0f, 1.0f, 0.0f);
+    drawCube();
+
+    glutSwapBuffers();
+
+    anguloCuboX+=0.1f;
+    anguloCuboY+=0.1f;
+    anguloEsfera+=0.2f;
+
+}
+
+void init()
+{
+   glClearColor(0,0,0,0);
+   glEnable(GL_DEPTH_TEST);
+   ancho = 400;
+   alto = 400;
+}
+
+void idle()
+{
+     display();
+ }
+
+void keyboard(unsigned char key, int x, int y)
+{
+    switch(key)
+    {
+    case 'p':
+    case 'P':
+      hazPerspectiva=1;
+      reshape(ancho,alto);
+      break;
+
+    case 'o':
+    case 'O':
+      hazPerspectiva=0;
+      reshape(ancho,alto);
+      break;
+
+    case 27:   // escape
+      exit(0);
+      break;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(400, 400);
+    glutCreateWindow("F I G U R A S");
+    init();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutIdleFunc(idle);
+    glutKeyboardFunc(keyboard);
+    glutMainLoop();
+    return 0;
+}
+
